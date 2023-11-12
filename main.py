a41d5026c49023fa6aa9166ec04f99926115e692
huntekah <huntekah@gmail.com>
2023-11-12 22:30:42 +0100

    fix doubling result file. Modify match lagorithm.

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from nlp import split_to_sentences, get_embeddings, sentence_transformer, compare_embeddings
 import torch
 from tqdm import tqdm
 
+from nlp import (compare_embeddings, get_embeddings, sentence_transformer,
+                 split_to_sentences)
+
+
 def match_sentences(ch_known, ch_learn):
     book = ""
-    lg_known = 'pl'
-    lg_learn = 'de'
+    lg_known = "pl"
+    lg_learn = "de"
+    window_size = 3
+    DIMINISH_FACTOR = 0.1
+
+    f_name = "ch4depl.txt"
+    with open(f_name, "w"):
+        pass
+    f_handle = open(f_name, "a+")
+
     print(len(ch_known), len(ch_learn))
     sents_learn = split_to_sentences(ch_learn[:], lg_learn)
     sents_known = split_to_sentences(ch_known[:], lg_known)
@@ -17,18 +28,51 @@ def match_sentences(ch_known, ch_learn):
 
     sims = compare_embeddings(embeddings_learn, embeddings_known)
     # print(sims)
+    last_k = 0
+    last_l = 0
+    for l, sim in tqdm(enumerate(sims)):
+        k_min = max(last_k - window_size, 0)
+        k_max = min(last_k + window_size, len(sim) - 1)
+        k_mid = (k_min + k_max) // 2
+        # cause of that I dont need full nxn embeddings matrix. to be fixed later
 
-    for l,sim in tqdm(enumerate(sims)):
+        for k_diminish in list(range(k_min)) + list(range(k_max, len(sim))):
+            sim[k_diminish] = sim[k_diminish] / (
+                1 + abs(k_diminish - k_mid) * DIMINISH_FACTOR
+            )
+        # sim[:k_min] = 0
+        # sim[k_max:] = 0
+        # print(sim)
         k = torch.argmax(sim)
-        # print(f"{sim[k]}\n{l}:{repr(sents_learn[l])}\n{k}:{repr(sents_known[k])}\n\n")
-        print(f"{l}[{lg_learn}]:{sents_learn[l]}\n{k}[{lg_known}]:{sents_known[k]}\n", file=open("ch4depl.txt","a+"))
+        if k > last_k:
+            print("+", l, int(k))
+            # print(f"{sim[k]}\n{l}:{repr(sents_learn[l])}\n{k}:{repr(sents_known[last_k:k+1])}\n\n")
+            learn_sentences_to_print = "".join(sents_learn[last_l:l])
+            known_sentences_to_print = "".join(sents_known[last_k:k])
+            print(
+                f"{last_l}-{l}[{lg_learn}]:{learn_sentences_to_print}\n{last_k}-{k}[{lg_known}]:{known_sentences_to_print}\n--",
+                file=f_handle,
+            )
+            last_k = k
+            last_l = l
+        else:
+            print(".", l, int(k))
+
+    learn_sentences_to_print = "".join(sents_learn[last_l:])
+    known_sentences_to_print = "".join(sents_known[last_k:])
+    print(
+        f"{last_l}-END[{lg_learn}]:{learn_sentences_to_print}\n{last_k}-END[{lg_known}]:{known_sentences_to_print}\n--",
+        file=f_handle,
+    )
+    f_handle.close()
 
 
 def main():
     ch4de = open("ch4de.txt", "r").read()
     ch4pl = open("ch4pl.txt", "r").read()
     ch4en = open("ch4en.txt", "r").read()
-    match_sentences(ch4pl,ch4de)
+    match_sentences(ch4pl, ch4de)
+
 
 if __name__ == "__main__":
     main()
--- nlp.py
+++ nlp.py
@@ -1,22 +1,28 @@
-import spacy
 from typing import List
+
+import spacy
 from sentence_transformers import SentenceTransformer, util
 
 nlp = {}
-nlp['en'] = spacy.load("en_core_web_sm")
-nlp['pl'] = spacy.load("pl_core_news_sm")
-nlp['de'] = spacy.load("de_core_news_sm")
+nlp["en"] = spacy.load("en_core_web_sm")
+nlp["pl"] = spacy.load("pl_core_news_sm")
+nlp["de"] = spacy.load("de_core_news_sm")
 sentence_transformer = SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
 
 
 def split_to_sentences(text, lang) -> List[str]:
     doc = nlp[lang](text)
-    return list(map(str,doc.sents))
+    return list(map(str, doc.sents))
+
+
 # doc = nlp('This is the first sentence. This is the second sentence.')
 # for sent in doc.sents:
-#     print(sent)
+#     print(senthttps://github.com/huntekah/parallel_potter)
+
 
 def compare_embeddings(emb1, emb2):
     return util.cos_sim(emb1, emb2)
+
+
 def get_embeddings(texts: List[str]):
     return sentence_transformer.encode(texts)
