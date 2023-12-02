6c25f28d7764d796a7c980d87fc5ed2385aaf4d5
huntekah <huntekah@gmail.com>
2023-12-02 21:49:57 +0100

    refactor with gpt

--- main.py
+++ main.py
@@ -11,74 +11,63 @@ def match_by_sim_score() -> List[Tuple[int,int]]:
     pass
     # Match by sim score, and then locate non-sequential
 
-def match_sentences(ch_known, ch_learn):
-    book = ""
+
+def prepare_file(chapter_n, lg_know, lg_learn):
+    f_name = f"chapters/ch{chapter_n}{lg_know}{lg_learn}.txt"
+    return open(f_name, "a+")
+
+
+def process_sentences(ch_text, lg_text):
+    return split_to_sentences(ch_text[:], lg_text)
+
+
+def calculate_similarity(embeddings_learn, embeddings_known, window_size, DIMINISH_FACTOR):
+    sims = compare_embeddings(embeddings_learn, embeddings_known)
+    last_k = 0
+    last_l = 0
+    matches = []
+    for l, sim in tqdm(enumerate(sims)):
+        k_min = max(last_k - window_size, 0)
+        k_max = min(last_k + window_size, len(sim) - 1)
+        k_mid = (k_min + k_max) // 2
+
+        for k_diminish in list(range(k_min)) + list(range(k_max, len(sim))):
+            sim[k_diminish] = sim[k_diminish] / (1 + abs(k_diminish - k_mid) * DIMINISH_FACTOR)
+
+        k = torch.argmax(sim)
+        if k > last_k:
+            matches.append((last_l, l, last_k, k))
+            last_k = k
+            last_l = l
+    matches.append((last_l, len(sims), last_k, len(sim)))
+    return matches
+
+
+def print_matches(f_handle, matches, sents_learn, sents_known, lg_learn, lg_known):
+    for last_l, l, last_k, k in matches:
+        learn_sentences_to_print = f"\n[{lg_learn}] ".join(sents_learn[last_l:l])
+        known_sentences_to_print = f"\n[{lg_known}] ".join(sents_known[last_k:k])
+        printable_sents = f"{last_k}-{k}[{lg_known}]:{known_sentences_to_print}\n{last_l}-{l}[{lg_learn}]:{learn_sentences_to_print}\n--"
+        print(printable_sents, file=f_handle)
+
+
+def match_sentences(ch_known, ch_learn, chapter_n):
     lg_known = "pl"
     lg_learn = "de"
     window_size = 3
     DIMINISH_FACTOR = 0.1
 
-    f_name = f"chapters/ch{chapter_n}depl.txt"
-    with open(f_name, "w"):
-        pass
-    f_handle = open(f_name, "a+")
+    with prepare_file(chapter_n,lg_know=lg_known,lg_learn=lg_learn) as f_handle:
+        sents_learn = split_to_sentences(ch_learn, lg_learn)
+        sents_known = split_to_sentences(ch_known, lg_known)
 
-    sents_learn = split_to_sentences(ch_learn[:], lg_learn)
-    sents_known = split_to_sentences(ch_known[:], lg_known)
+        embeddings_learn = get_embeddings(sents_learn)
+        embeddings_known = get_embeddings(sents_known)
 
-    embeddings_learn = get_embeddings(sents_learn)
-    embeddings_known = get_embeddings(sents_known)
-    print(f"{len(embeddings_learn)=}")
-    print(f"{len(embeddings_known)=}")
+        matches = calculate_similarity(embeddings_learn, embeddings_known, window_size, DIMINISH_FACTOR)
 
-    sims = compare_embeddings(embeddings_learn, embeddings_known)
-    # print(sims)
-    last_k = 0
-    last_l = 0
-    for l, sim in tqdm(enumerate(sims)):
-        k_min = max(last_k - window_size, 0)
-        k_max = min(last_k + window_size, len(sim) - 1)
-        k_mid = (k_min + k_max) // 2
-        # cause of that I dont need full nxn embeddings matrix. to be fixed later
+        print_matches(f_handle, matches, sents_learn, sents_known, lg_learn, lg_known)
 
-        for k_diminish in list(range(k_min)) + list(range(k_max, len(sim))):
-            sim[k_diminish] = sim[k_diminish] / (
-                1 + abs(k_diminish - k_mid) * DIMINISH_FACTOR
-            )
-        # sim[:k_min] = 0
-        # sim[k_max:] = 0
-        # print(sim)
-        k = torch.argmax(sim)
-        if k > last_k:
-            print("+", l, int(k))
-            # print(f"{sim[k]}\n{l}:{repr(sents_learn[l])}\n{k}:{repr(sents_known[last_k:k+1])}\n\n")
-            learn_sentences_to_print = f"\n[{lg_learn}] ".join(sents_learn[last_l:l])
-            known_sentences_to_print = f"\n[{lg_known}] ".join(sents_known[last_k:k])
-            printable_sents = f"""{last_k}-{k}[{lg_known}]:{known_sentences_to_print}
-{last_l}-{l}[{lg_learn}]:{learn_sentences_to_print}
---"""
-            print(printable_sents, file=f_handle)
-            # print(
-            #     f"{last_l}-{l}[{lg_learn}]:{learn_sentences_to_print}\n{last_k}-{k}[{lg_known}]:{known_sentences_to_print}\n--",
-            #     file=f_handle,
-            # )
-            last_k = k
-            last_l = l
-        else:
-            print(".", l, int(k))
-
-    learn_sentences_to_print = "".join(sents_learn[last_l:])
-    known_sentences_to_print = "".join(sents_known[last_k:])
-    printable_sents = f"""{last_k}-END[{lg_known}]:{known_sentences_to_print}
-{last_l}-END[{lg_learn}]:{learn_sentences_to_print}
---"""
-    print(printable_sents, file=f_handle)
-
-    # print(
-    #         f"{last_l}-END[{lg_learn}]:{learn_sentences_to_print}\n{last_k}-END[{lg_known}]:{known_sentences_to_print}\n--",
-    #         file=f_handle,
-    #     )
-    f_handle.close()
 
 
 def main():
